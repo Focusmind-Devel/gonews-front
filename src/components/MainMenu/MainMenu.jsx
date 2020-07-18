51d5058f0e28e50a48a5ae35c9a07fd72282a618
Karen Gil <albanis21@gmail.com>
2020-07-18 12:56:00 -0400

    ajustes link del menu cuando se encuentra en la nota 2

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -155,7 +155,7 @@ const MainMenu = ({ menuOpen, isOpen }) => {
                     key={index}
                     className='menu_item adicional animate__animated animate__fadeInRight animate__faster'
                   >
-                    <Link className='click' to={`${item.slug}`}>
+                    <Link className='click' to={`/${item.slug}`}>
                       {item.name}
                     </Link>
                   </li>
