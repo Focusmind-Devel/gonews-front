dc2d4893f9c99cb147e959a88635e7d28064980e
Karen Gil <albanis21@gmail.com>
2020-07-20 11:24:57 -0400

    ajustes index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import NotasState from './context/notas/NotasState';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <Router>
-      <NotasState>
-        <App />
-      </NotasState>
-    </Router>,
-    rootElement
-  );
-} else {
-  render(
-    <Router>
-      <NotasState>
-        <App />
-      </NotasState>
-    </Router>,
-    rootElement
-  );
-}
+ReactDOM.render(
+  <Router>
+    <NotasState>
+      <App />
+    </NotasState>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
