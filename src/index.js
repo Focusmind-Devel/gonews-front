7b1198ece3f379d893ffadb6b298f14a24f0adaf
Karen Gil <albanis21@gmail.com>
2020-08-21 19:48:30 -0400

    ajustes react-snap3

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ if (rootElement.hasChildNodes()) {
 		rootElement
 	);
 }
-
+/*
 // ReactDOM.render(
 render(
 	<Router>
@@ -39,7 +39,7 @@ render(
 	document.getElementById('root')
 );
 // );
-
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
