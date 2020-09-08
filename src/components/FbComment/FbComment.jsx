61ed7c43637e79c724a63ffcf29ef6361e62049d
Karen Gil <albanis21@gmail.com>
2020-09-08 08:36:57 -0400

    ajustes de FB Comments 2

--- src/components/FbComment/FbComment.jsx
+++ src/components/FbComment/FbComment.jsx
@@ -9,7 +9,7 @@ const FbComment = ({ slug }) => {
 		<div className='ancho-comentario'>
 			<div
 				className='fb-comments'
-				data-href={`https://gonews.com.ar/${slug}`}
+				data-href={`https://gonews.com.ar/nota/${slug}`}
 				data-numposts='5'
 				data-width='80%'
 			></div>
